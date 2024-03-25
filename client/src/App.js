821a9360ea5c41ca0aed8133cf1808d5c5321009
Omor Riduan <93811768+smRid@users.noreply.github.com>
2024-03-26 00:43:32 +0600

    Code update

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-// import 'antd/dist/antd.css'
+import 'antd/dist/antd.min.css'
 import {Button} from 'antd'
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,9 +1,31 @@
+import { form } from 'antd'
+import Input from 'antd/lib/input/Input'
 import React from 'react'
 
 function Register() {
   return (
-    <div>
-      <h1>Reg</h1>
+    <div className='register'>
+      <div className='row'>
+        <div className='col-md-5'></div>
+        <div className='col-md-5'>
+          <Form>
+            <Form.item label='Name' name='name'>
+              <Input></Input>
+            </Form.item>
+
+            <Form.item label='Email' name='email'>
+              <Input></Input>
+            </Form.item>
+
+            <Form.item label='Password' name='password'>
+              <Input></Input>
+            </Form.item>
+
+
+          </Form>
+        </div>
+      </div>
+      
     </div>
   )
 }
