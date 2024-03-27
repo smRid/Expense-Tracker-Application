d137bd5ebb76bc8174fbdca5b9584839a97747f5
Turya99 <141634354+Turya99@users.noreply.github.com>
2024-03-27 13:20:40 +0600

    Update Login.js

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,11 +1,56 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import { Form, Input, message } from "antd";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
+import Spinner from "../components/Spinner";
+const Login = () => {
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+  //from submit
+  const submitHandler = async (values) => {
+    try {
+      setLoading(true);
+      const { data } = await axios.post("/users/login", values);
+      setLoading(false);
+      message.success("login success");
+      localStorage.setItem(
+        "user",
+        JSON.stringify({ ...data.user, password: "" })
+      );
+      navigate("/");
+    } catch (error) {
+      setLoading(false);
+      message.error("something went wrong");
+    }
+  };
 
-function Login() {
+  //prevent for login user
+  useEffect(() => {
+    if (localStorage.getItem("user")) {
+      navigate("/");
+    }
+  }, [navigate]);
   return (
-    <div>
-      <h1>Login</h1>
-    </div>
-  )
-}
+    <>
+      <div className="resgister-page ">
+        {loading && <Spinner />}
+        <Form layout="vertical" onFinish={submitHandler}>
+          <h1>Login Form</h1>
 
-export default Login
+          <Form.Item label="Email" name="email">
+            <Input type="email" />
+          </Form.Item>
+          <Form.Item label="Password" name="password">
+            <Input type="password" />
+          </Form.Item>
+          <div className="d-flex justify-content-between">
+            <Link to="/register">Not a user ? Cleck Here to regsiter</Link>
+            <button className="btn btn-primary">Login</button>
+          </div>
+        </Form>
+      </div>
+    </>
+  );
+};
+
+export default Login;
