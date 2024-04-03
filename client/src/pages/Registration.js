06e78a3a9fef988178a9a37e3cd4812dae4db00a
Sheikh Nafis <93274502+Sheikh-Nafis@users.noreply.github.com>
2024-04-03 13:02:07 +0600

    Registration.js

--- client/src/pages/Registration.js
+++ client/src/pages/Registration.js
@@ -1,15 +1,1 @@
-import React from 'react'
-import DefaultLayout from '../components/DefaultLayout'
 
-function Registration() {
-  return (
-    // Home component representing the home page
-    <DefaultLayout>
-
-        <h1>This is the Registration Page</h1>
-        {/* <Button type="primary">Primary Button</Button> */}
-    </DefaultLayout>
-  )
-}
-
-export default Registration
