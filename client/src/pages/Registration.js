1cb229969606e3d1e4d13daf1a074a5abd3ba5f2
Sheikh Nafis <sk.nafis33@yahoo.com>
2024-04-15 22:26:00 +0600

    working with registration

--- client/src/pages/Registration.js
+++ client/src/pages/Registration.js
@@ -1,16 +1,46 @@
 import React from 'react'
+import { Form, Input } from 'antd';
+import { Link } from 'react-router-dom'; 
 import DefaultLayout from '../components/DefaultLayout'
 
 function Registration() {
   return (
     // Home component representing the home page
-    <DefaultLayout>
+  <div className='register'>
+    <div className="row">
+        <div className="col-md-5">
 
-        <h1>This is the Registration Page</h1>
-        {/* <Button type="primary">Primary Button</Button> */}
-    </DefaultLayout>
+        </div>
+        <div className="col-md-5">
+          <Form layout='vertical'>
+              <Form.Item label='Name' name='name'>
+
+                  <Input/>
+
+              </Form.Item>
+
+              <Form.Item label='Email' name='email'>
+
+                  <Input/>
+
+              </Form.Item>
+
+              <Form.Item label='Password' name='password'>
+
+                  <Input/>
+
+              </Form.Item>
+
+              <div className="d-flex justify-content-between align-items-center">
+                <Link to='/login'>Already Registered? Click here to Login</Link>
+                <button className='primary'>Register</button>
+              </div>
+          </Form>
+          
+        </div>
+    </div>
+  </div>
   )
 }
 
 export default Registration
-
